763310395d6f123d06ba6d40da3c9fa874f89654
MdRoki2020 <mroki815@gmail.com>
2022-08-16 12:25:38 +0600

    User Controller Model & Routes

---
+++ inventory-mern-backend/src/controllers/Users/UsersControllers.js
@@ -0,0 +1,50 @@
+const DataModel = require("../../models/Users/UsersModel");
+const OTPSModel = require("../../models/Users/OTPSModel");
+const UserCreateService = require("../../services/user/UserCreateService");
+const UserLoginService = require("../../services/user/UserLoginService");
+const UserUpdateService = require("../../services/user/UserUpdateService");
+const UserDetailsService = require("../../services/user/UserDetailsService");
+const UserResetPassService = require("../../services/user/UserResetPassService");
+const UserVerifyOtpService = require("../../services/user/UserVerifyOtpService");
+const UserVerifyEmailService = require("../../services/user/UserVerifyEmailService");
+
+
+exports.Registration=async (req, res) => {
+    let Result=await UserCreateService(req,DataModel)
+    res.status(200).json(Result)
+}
+
+exports.Login=async(req,res)=>{
+    let Result=await UserLoginService(req,DataModel)
+    res.status(200).json(Result)
+}
+
+exports.ProfileUpdate=async (req, res) => {
+    let Result=await UserUpdateService(req,DataModel)
+    res.status(200).json(Result)
+}
+
+exports.ProfileDetails=async (req, res) => {
+    let Result=await UserDetailsService(req,DataModel)
+    res.status(200).json(Result)
+}
+
+
+exports.RecoverVerifyEmail=async (req,res)=>{
+    let Result=await UserVerifyEmailService(req,DataModel)
+    res.status(200).json(Result)
+}
+
+
+exports.RecoverVerifyOTP=async (req,res)=>{
+    let Result=await UserVerifyOtpService(req,OTPSModel)
+    res.status(200).json(Result)
+}
+
+exports.RecoverResetPass=async (req,res)=>{
+    let Result=await UserResetPassService(req,DataModel)
+    res.status(200).json(Result)
+}
+
+
+
--- inventory-mern-backend/src/routes/api.js
+++ inventory-mern-backend/src/routes/api.js
@@ -1,6 +1,18 @@
 const express=require("express");
-
-
+const AuthVerifyMiddleware=require("../middlewares/AuthVerifyMiddleware");
+const UsersController=require('../controllers/Users/UsersControllers')
 const router=express.Router();
+
+//USER PROFILE...
+router.post("/Registration",UsersController.Registration);
+router.post("/Login",UsersController.Login);
+router.post("/ProfileUpdate",AuthVerifyMiddleware,UsersController.ProfileUpdate);
+router.get("/ProfileDetails",AuthVerifyMiddleware,UsersController.ProfileDetails);
+router.get("/RecoverVerifyEmail/:email",UsersController.RecoverVerifyEmail);
+router.get("/RecoverVerifyOTP/:email/:otp",UsersController.RecoverVerifyOTP);
+router.post("/RecoverResetPass",UsersController.RecoverResetPass);
+
+
+
 
 module.exports=router;
