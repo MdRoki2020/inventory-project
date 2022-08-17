9e02f3f76a403ee4da90c32dd9e12ad841fe7b3a
MdRoki2020 <mroki815@gmail.com>
2022-08-17 23:40:16 +0600

    Categories Suppliers Customers api

---
+++ inventory-mern-backend/src/controllers/Suppliers/SuppliersController.js
@@ -0,0 +1,27 @@
+const DataModel = require("../../models/Suppliers/SuppliersModel");
+const CreateService = require("../../services/common/CreateService");
+const UpdateService = require("../../services/common/UpdateService");
+const ListService = require("../../services/common/ListService");
+const DropDownService = require("../../services/common/DropDownService");
+
+exports.CreateSuppliers=async (req, res) => {
+    let Result= await CreateService(req,DataModel)
+    res.status(200).json(Result)
+}
+
+exports.UpdateSuppliers=async (req, res) => {
+    let Result=await UpdateService(req,DataModel)
+    res.status(200).json(Result)
+}
+
+exports.SuppliersList=async (req, res) => {
+    let SearchRgx = {"$regex": req.params.searchKeyword, "$options": "i"}
+    let SearchArray=[{Name: SearchRgx},{Phone: SearchRgx},{Email: SearchRgx},{Address: SearchRgx}]
+    let Result= await ListService(req,DataModel,SearchArray)
+    res.status(200).json(Result)
+}
+
+exports.SuppliersDropDown=async (req, res) => {
+    let Result= await DropDownService(req,DataModel,{_id:1,Name:1})
+    res.status(200).json(Result)
+}
--- inventory-mern-backend/src/routes/api.js
+++ inventory-mern-backend/src/routes/api.js
@@ -2,6 +2,9 @@ const express=require("express");
 const AuthVerifyMiddleware=require("../middlewares/AuthVerifyMiddleware");
 const UsersController=require('../controllers/Users/UsersControllers')
 const BrandsController=require('../controllers/Brands/BrandsController')
+const CategoriesController=require('../controllers/Categories/CategoriesController')
+const CustomersController=require('../controllers/Customers/CustomersController')
+const SuppliersController=require('../controllers/Suppliers/SuppliersController')
 const router=express.Router();
 
 //USER PROFILE...
@@ -20,6 +23,25 @@ router.post("/UpdateBrand/:id",AuthVerifyMiddleware,BrandsController.UpdateBrand
 router.get("/BrandList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,BrandsController.BrandList);
 router.get("/BrandDropDown",AuthVerifyMiddleware,BrandsController.BrandDropDown);
 
+// Categories
+router.post("/CreateCategories",AuthVerifyMiddleware,CategoriesController.CreateCategories);
+router.post("/UpdateCategories/:id",AuthVerifyMiddleware,CategoriesController.UpdateCategories);
+router.get("/CategoriesList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,CategoriesController.CategoriesList);
+router.get("/CategoriesDropDown",AuthVerifyMiddleware,CategoriesController.CategoriesDropDown);
+
+
+// Customers
+router.post("/CreateCustomers",AuthVerifyMiddleware,CustomersController.CreateCustomers);
+router.post("/UpdateCustomers/:id",AuthVerifyMiddleware,CustomersController.UpdateCustomers);
+router.get("/CustomersList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,CustomersController.CustomersList);
+router.get("/CustomersDropDown",AuthVerifyMiddleware,CustomersController.CustomersDropDown);
+
+
+// Suppliers
+router.post("/CreateSuppliers",AuthVerifyMiddleware,SuppliersController.CreateSuppliers);
+router.post("/UpdateSuppliers/:id",AuthVerifyMiddleware,SuppliersController.UpdateSuppliers);
+router.get("/SuppliersList/:pageNo/:perPage/:searchKeyword",AuthVerifyMiddleware,SuppliersController.SuppliersList);
+router.get("/SuppliersDropDown",AuthVerifyMiddleware,SuppliersController.SuppliersDropDown);
 
 
 module.exports=router;
