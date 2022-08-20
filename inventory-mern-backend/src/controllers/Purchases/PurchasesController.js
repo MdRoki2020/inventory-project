be1dcd17398a20c6841f485d2f97dd5377355b15
MdRoki2020 <mroki815@gmail.com>
2022-08-20 19:48:33 +0600

    PurchasesList

--- inventory-mern-backend/src/controllers/Purchases/PurchasesController.js
+++ inventory-mern-backend/src/controllers/Purchases/PurchasesController.js
@@ -1,7 +1,7 @@
 const ParentModel = require("../../models/Purchases/PurchasesModel");
 const ChildsModel = require("../../models/Purchases/PurchaseProductsModel");
 const CreateParentChildsService = require("../../services/common/CreateParentChildService");
-// const ListOneJoinService = require("../../services/common/ListOneJoinService");
+const ListOneJoinService = require("../../services/common/ListOneJoinService");
 // const DeleteParentChildsService = require("../../services/common/DeleteParentChildsService");
 
 
@@ -10,13 +10,13 @@ exports.CreatePurchases=async (req, res) => {
     res.status(200).json(Result)
 }
 
-// exports.PurchasesList=async (req, res) => {
-//     let SearchRgx = {"$regex": req.params.searchKeyword, "$options": "i"}
-//     let JoinStage={$lookup: {from: "suppliers", localField: "SupplierID", foreignField: "_id", as: "suppliers"}};
-//     let SearchArray=[{Note: SearchRgx},{'suppliers.Name': SearchRgx},{'suppliers.Address': SearchRgx},{'suppliers.Phone': SearchRgx},{'suppliers.Email': SearchRgx}]
-//     let Result=await ListOneJoinService(req,ParentModel,SearchArray,JoinStage);
-//     res.status(200).json(Result)
-// }
+exports.PurchasesList=async (req, res) => {
+    let SearchRgx = {"$regex": req.params.searchKeyword, "$options": "i"}
+    let JoinStage={$lookup: {from: "suppliers", localField: "SupplierID", foreignField: "_id", as: "suppliers"}};
+    let SearchArray=[{Note: SearchRgx},{'suppliers.Name': SearchRgx},{'suppliers.Address': SearchRgx},{'suppliers.Phone': SearchRgx},{'suppliers.Email': SearchRgx}]
+    let Result=await ListOneJoinService(req,ParentModel,SearchArray,JoinStage);
+    res.status(200).json(Result)
+}
 
 // exports.PurchasesDelete=async (req, res) => {
 //     let Result=await  DeleteParentChildsService(req,ParentModel,ChildsModel,'PurchaseID')
