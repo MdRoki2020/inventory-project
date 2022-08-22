c51fe06471ed54b158efa115781f4b7230975c94
MdRoki2020 <mroki815@gmail.com>
2022-08-22 23:13:26 +0600

    add comment create token

--- inventory-mern-backend/src/utility/CreateToken.js
+++ inventory-mern-backend/src/utility/CreateToken.js
@@ -1,5 +1,6 @@
 const jwt=require("jsonwebtoken");
 
+//create token..
 const CreateToken=async (data) => {
     let payload={exp:Math.floor(Date.now()/1000)+(24*60*60), data:data}
     return await jwt.sign(payload,'SecretKey123456789');
