06c4b451e3b589f4745b6f43042df259330c66a3
MdRoki2020 <mroki815@gmail.com>
2022-11-02 21:34:36 +0600

    update routing

--- inventory-mern-backend/src/controllers/Expenses/ExpenseTypesController.js
+++ inventory-mern-backend/src/controllers/Expenses/ExpenseTypesController.js
@@ -3,6 +3,8 @@ const CreateService = require("../../services/common/CreateService");
 const UpdateService = require("../../services/common/UpdateService");
 const ListService = require("../../services/common/ListService");
 const DropDownService = require("../../services/common/DropDownService");
+const CheckAssociateService = require("../../services/common/CheckAssociateService");
+const DeleteService = require("../../services/common/DeleteService");
 
 exports.CreateExpenseTypes=async (req, res) => {
     let Result= await CreateService(req,DataModel)
@@ -24,4 +26,18 @@ exports.ExpenseTypesList=async (req, res) => {
 exports.ExpenseTypesDropDown=async (req, res) => {
     let Result= await DropDownService(req,DataModel,{_id:1,Name:1})
     res.status(200).json(Result)
+}
+
+
+exports.DeleteExpenseTypes=async (req, res) => {
+    let DeleteID=req.params.id;
+    const ObjectId = mongoose.Types.ObjectId;
+    let CheckAssociate= await CheckAssociateService({TypeID:ObjectId(DeleteID)},ExpensesModel);
+    if(CheckAssociate){
+        res.status(200).json({status: "associate", data: "Associate with Expenses"})
+    }
+    else{
+        let Result=await DeleteService(req,DataModel);
+        res.status(200).json(Result)
+    }
 }
--- inventory-mern-backend/src/controllers/Suppliers/SuppliersController.js
+++ inventory-mern-backend/src/controllers/Suppliers/SuppliersController.js
@@ -3,6 +3,8 @@ const CreateService = require("../../services/common/CreateService");
 const UpdateService = require("../../services/common/UpdateService");
 const ListService = require("../../services/common/ListService");
 const DropDownService = require("../../services/common/DropDownService");
+const CheckAssociateService = require("../../services/common/CheckAssociateService");
+const DeleteService = require("../../services/common/DeleteService");
 
 exports.CreateSuppliers=async (req, res) => {
     let Result= await CreateService(req,DataModel)
@@ -24,4 +26,18 @@ exports.SuppliersList=async (req, res) => {
 exports.SuppliersDropDown=async (req, res) => {
     let Result= await DropDownService(req,DataModel,{_id:1,Name:1})
     res.status(200).json(Result)
+}
+
+
+exports.DeleteSupplier=async (req, res) => {
+    let DeleteID=req.params.id;
+    const ObjectId = mongoose.Types.ObjectId;
+    let CheckAssociate= await CheckAssociateService({SupplierID:ObjectId(DeleteID)},PurchasesModel);
+    if(CheckAssociate){
+        res.status(200).json({status: "associate", data: "Associate with Purchases"})
+    }
+    else{
+        let Result=await DeleteService(req,DataModel);
+        res.status(200).json(Result)
+    }
 }
